54d1ce5c695a0da0fcb966d53f5ff272e308bf5a
kuloud <kuloud@outlook.com>
2026-01-18 18:16:03 +0800

    build: fix basename for github pages

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,19 +3,24 @@ import AppLayout from "@/app/layout";
 import HomePage from "@/app/page";
 import NotFoundPage from "@/app/not-found/page";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
-      },
-    ],
+    basename: "/ui-effect-demo",
   },
-]);
+);
